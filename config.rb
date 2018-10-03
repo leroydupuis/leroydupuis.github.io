3d5691a7cddec8ac6f3a165c19bd7b01aeb536fc
leroydupuis <iamleroydupuis@gmail.com>
2018-10-03 17:21:55 +0530

    added generated files

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/leroydupuis.gi/"
+http_images_path = "/leroydupuis.gi/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/leroydupuis.gi/fonts"
+css_dir = "public/leroydupuis.gi/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
